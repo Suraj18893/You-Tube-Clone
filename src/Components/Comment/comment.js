f5e619592d4078d74da301eec999f547e75a3457
Suraj Ganguly <surajganguly18@gmail.com>
2021-12-08 23:56:14 +0530

    Update comment.js

--- src/Components/Comment/comment.js
+++ src/Components/Comment/comment.js
@@ -2,7 +2,9 @@ import React from "react";
 import "./_comment.scss";
 import moment from "moment";
 
-
+const comment = ({ comment }) => {
+  const { authorDisplayName, authorProfileImageUrl, publishedAt, textDisplay } =
+    comment;
 
   return (
     <div className="p-2 comment d-flex">
